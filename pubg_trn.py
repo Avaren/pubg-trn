68f8c348120b322c002c48f0200c4418bb4bae2b
Ian Salmons <ian.salmons@gmail.com>
2017-07-12 23:03:42 +0100

    Add limits

--- pubg_trn.py
+++ pubg_trn.py
@@ -2,6 +2,8 @@ import asyncio
 
 import aiohttp
 
+from ..limit import Limit
+
 API_ROOT = 'https://pubgtracker.com/api'
 API_PROFILE = '{}/profile/{{}}/{{}}'.format(API_ROOT)
 API_SEARCH = '{}/search'.format(API_ROOT)
@@ -20,6 +22,7 @@ class PUBGTRN:
             'trn-api-key': key,
         }
 
+    @Limit(1, 1)
     async def search(self, id, mode='steamId'):
         async with self.session.get(API_SEARCH, params={mode: id}, headers=self.headers) as req:
             if req.status == 200:
@@ -27,6 +30,7 @@ class PUBGTRN:
             else:
                 raise NotFound()
 
+    @Limit(1, 1)
     async def profile(self, name, platform='pc'):
         async with self.session.get(API_PROFILE.format(platform, name), headers=self.headers) as req:
             if req.status == 200:
